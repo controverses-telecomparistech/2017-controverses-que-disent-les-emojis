548d85a7ac2485cf5ea3700e158fdbbaae69620d
szhang <samory94326@gmail.com>
2017-06-08 18:15:43 +0200

    add some documents for design

---
+++ Design /Arbre_debat/emoji_arbreArgument.mm
@@ -0,0 +1,114 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+    <node CREATED="1496171314922" ID="ID_6b73695453d1206df4b7cee1da207cd1" MODIFIED="1496171314922" TEXT="emoji" >
+            <node CREATED="1496171314922" ID="ID_6b73695453d1206df4b7cee1da207cd1" MODIFIED="1496171314922" TEXT="langage" POSITION="right">
+            <node CREATED="1496171314922" ID="ID_6b73695453d1206df4b7cee1da207cd1" MODIFIED="1496171314922" TEXT="utilisation" POSITION="right">
+            <node CREATED="1496171314922" ID="ID_6b73695453d1206df4b7cee1da207cd1" MODIFIED="1496171314922" TEXT="fréquence" POSITION="right">
+            <node CREATED="1496171314922" ID="ID_6b73695453d1206df4b7cee1da207cd1" MODIFIED="1496171314922" TEXT="jauger le trop, pas assez" POSITION="right">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="information sur la personne" POSITION="right">
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="influence sur la signification" POSITION="right">
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="contexte" POSITION="right">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="quand l'utilisez?  -sms  -email" POSITION="right">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="langage familière" POSITION="right">
+    </node>
+
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="est ce que un langage?" POSITION="right">
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="appauvrissement ou enrichissement?" POSITION="right">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="aide à la communication?" POSITION="right">
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="concurrence le texte" POSITION="right">
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="diversité" POSITION="left">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="normes" POSITION="left">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="requêtes" POSITION="left">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="tout acceptez?" POSITION="left">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="cq: emoji au profit d'autres caractères" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="quotas?" POSITION="left">
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="peut on arriver à trop de diversité? " POSITION="left">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="discrimination(pas assez)" POSITION="left">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="couleur" POSITION="left">
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="famile" POSITION="left">
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="homme/femme" POSITION="left">
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="cq: exclusion et non utilisation" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="pertinence" POSITION="left">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="plus d'information" POSITION="left">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="réaction facebook" POSITION="left">
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="cq: non utilisateur de certains" POSITION="left">
+    </node>
+
+    </node>
+
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="detournement" POSITION="right">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="censure" POSITION="right">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="instagram" POSITION="right">
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="stratégie de communication" POSITION="right">
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="campagne politique" POSITION="right">
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="pubicité" POSITION="right">
+    </node>
+
+    </node>
+
+            <node CREATED="1496171314923" ID="ID_ba66cac0fa3290c38922bd3df7e7fe32" MODIFIED="1496171314923" TEXT="si l'emoji en question existait, aurait il été utilisé?" POSITION="right">
+    </node>
+
+    </node>
+
+    </node>
+
+</map>
---
+++ Design /carte_acteurs/Emoji debat.mm
@@ -0,0 +1,151 @@
+
+<map version="0.9.0">
+    <node TEXT="Emoji debat" FOLDED="false">
+        <edge COLOR="#272727" />
+        <font NAME="Helvetica" SIZE="10" />
+        <node TEXT="Langage" FOLDED="false" POSITION="right">
+            <edge COLOR="#988ee3" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Utilisation" FOLDED="false">
+                <edge COLOR="#9991e2" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="A quelle fréquence?" FOLDED="false">
+                    <edge COLOR="#9b94e3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Jauger le trop, pas assez" FOLDED="false">
+                        <edge COLOR="#9892e3" />
+                        <font NAME="Helvetica" SIZE="10" />
+                        <node TEXT="Information sur la personne" FOLDED="false">
+                            <edge COLOR="#8e89e0" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                        <node TEXT="Influence sur la signification" FOLDED="false">
+                            <edge COLOR="#9590e3" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                    </node>
+                </node>
+                <node TEXT="Dans quels contextes?" FOLDED="false">
+                    <edge COLOR="#9b94e5" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Plutôt dans un SMS que dans un mail" FOLDED="false">
+                        <edge COLOR="#9a95e6" />
+                        <font NAME="Helvetica" SIZE="10" />
+                        <node TEXT="Langage plus familier que professionnel " FOLDED="false">
+                            <edge COLOR="#9591e6" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                    </node>
+                </node>
+            </node>
+            <node TEXT="Est ce que un langage?" FOLDED="false">
+                <edge COLOR="#8f86e3" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Appauvrissement ou enrichissement?" FOLDED="false">
+                <edge COLOR="#978ee3" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Aide à la communication?" FOLDED="false">
+                    <edge COLOR="#938be4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Concurrence le texte" FOLDED="false">
+                    <edge COLOR="#8f87e2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="Diversité" FOLDED="false" POSITION="left">
+            <edge COLOR="#efa670" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Normes" FOLDED="false">
+                <edge COLOR="#f0aa79" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Requêtes" FOLDED="false">
+                    <edge COLOR="#f0ac7f" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Tout accepter?" FOLDED="false">
+                        <edge COLOR="#efa87c" />
+                        <font NAME="Helvetica" SIZE="10" />
+                        <node TEXT="emojis détriment d'autres caractères" FOLDED="false">
+                            <edge COLOR="#f0a67b" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                    </node>
+                    <node TEXT="Quotas?" FOLDED="false">
+                        <edge COLOR="#efa87c" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+            </node>
+            <node TEXT="Peut on arriver à trop de diversité? " FOLDED="false">
+                <edge COLOR="#efa06a" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Discrimination(pas assez)" FOLDED="false">
+                    <edge COLOR="#f0a473" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Couleur" FOLDED="false">
+                        <edge COLOR="#f09a67" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Famile" FOLDED="false">
+                        <edge COLOR="#f1a374" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Homme/femme" FOLDED="false">
+                        <edge COLOR="#f2a87b" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Cq: exclusion et non utilisation" FOLDED="false">
+                        <edge COLOR="#f1a97e" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+                <node TEXT="Pertinence" FOLDED="false">
+                    <edge COLOR="#f2a473" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Plus d'information" FOLDED="false">
+                        <edge COLOR="#f29d6b" />
+                        <font NAME="Helvetica" SIZE="10" />
+                        <node TEXT="Réactions Facebook" FOLDED="false">
+                            <edge COLOR="#f3a276" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                    </node>
+                    <node TEXT="Non utilisation de certains" FOLDED="false">
+                        <edge COLOR="#ef9b6a" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+            </node>
+        </node>
+        <node TEXT="Détournement?" FOLDED="false" POSITION="right">
+            <edge COLOR="#e8e525" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Une censure est-elle nécessaire? " FOLDED="false">
+                <edge COLOR="#e8e22f" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Instagram a interdit la recherche de l'emoji aubergine " FOLDED="false">
+                    <edge COLOR="#e9df24" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Même si certains emojis existait, auraient-ils été utilisés?" FOLDED="false">
+                <edge COLOR="#e7e120" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Stratégie de communication" FOLDED="false">
+                <edge COLOR="#e8e11f" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Campagne politique" FOLDED="false">
+                    <edge COLOR="#e7dd26" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Pubicité" FOLDED="false">
+                    <edge COLOR="#e6dc27" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+    </node>
+</map>
